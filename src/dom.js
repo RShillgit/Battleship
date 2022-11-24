93d5c867cb1d558e456c6faba0abf37de42a1a5c
Robert Siciliano <Robert.siciliano22@gmail.com>
2022-11-24 12:52:22 -0500

    Beginning of Ship Drag & Drop

--- src/dom.js
+++ src/dom.js
@@ -39,10 +39,7 @@ function eventHandler(e) {
     if (e.target.innerHTML === 'Ships') return shipsScreen();
 
     // Vessels
-    if (e.currentTarget.className.includes('vessel')) {
-        // TODO: Somehow allow user to drag the Vessel onto the board
-        console.log(e.currentTarget.id);
-    }
+    if (e.currentTarget.className.includes('vessel')) return shipSelect(e);
 }
 
 function shipsScreen() {
@@ -53,7 +50,51 @@ function shipsScreen() {
 
     // Add event listener to each vessel
     const vessels = document.querySelectorAll('.vessel');
-    vessels.forEach(vessel => vessel.addEventListener('mousedown', (e) => eventHandler(e)));
+
+    // User Board Boxes
+    const userBoardBoxes = document.getElementById('usersGameboard').querySelectorAll('.gameBoardBox');
+    
+    vessels.forEach(vessel => {
+
+        // Array to hold the coordinates hovered over, that way we can pick the last coordinate to place the ship
+        let coords = [];
+
+        // Drag start event
+        vessel.addEventListener('dragstart', (e) => {
+
+            // Add a hover event to the user board boxes
+            userBoardBoxes.forEach(userBox => {
+                
+                userBox.addEventListener('dragenter', (e) => {
+                    e.preventDefault();
+
+                    // Push coordinate to array
+                    coords.push(e.target.id);
+                    console.log(coords);
+                })
+                userBox.addEventListener('dragover', (e) => {
+                    e.preventDefault();
+                    e.target.style.backgroundColor = 'red';
+                })
+            });
+        });
+
+        vessel.addEventListener('dragend', (e) => {
+            console.log(coords);
+        })
+
+    });
+}
+
+function shipSelect(e) {
+
+
+}
+
+function shipDrag(e) {
+
+    
+    e.dataTransfer.setData('text', e.target.id);
 }
 
 export {
